c5a14fdebb7bf3e79df298577f3cdf31ee5bf44f
Alexander Wahl-Rasmussen <a.wahl.r@gmail.com>
2019-12-06 20:10:31 +0100

    refactored test struct

--- day1/main_test.go
+++ day1/main_test.go
@@ -1,6 +1,7 @@
 package day1
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -11,15 +12,14 @@ func calculateFuel(mass int) int {
 
 func TestCalculateFuel(t *testing.T) {
 	tests := []struct {
-		name     string
 		expected int
 		mass     int
 	}{
-		{name: "mass 12", expected: 2, mass: 12},
-		{name: "mass 14", expected: 2, mass: 14},
+		{expected: 2, mass: 12},
+		{expected: 2, mass: 14},
 	}
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
+		t.Run(fmt.Sprintf("Mass %v", test.mass), func(t *testing.T) {
 			assert.Equal(t, test.expected, calculateFuel(test.mass))
 		})
 	}
