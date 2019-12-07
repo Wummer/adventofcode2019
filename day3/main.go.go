1ae3a70e401021d5ac0ac49286d5279f9375df91
Alexander Wahl-Rasmussen <a.wahl.r@gmail.com>
2019-12-07 17:59:27 +0100

    added solution for exercise 2

--- day3/main.go.go
+++ day3/main.go.go
@@ -2,16 +2,21 @@ package day3
 
 import (
 	"math"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-func ClosestIntersection(wire1 [][2]int, wire2 [][2]int) int {
+func ClosestIntersection(wire1 [][2]int, wire2 [][2]int) (int, int) {
 	var intersections [][2]int
-	for _, cWire1 := range wire1 {
-		for _, cWire2 := range wire2 {
+	var steps []int
+
+	for xStep, cWire1 := range wire1 {
+
+		for yStep, cWire2 := range wire2 {
 			if cWire1 == cWire2 && cWire1 != [2]int{0, 0} {
 				intersections = append(intersections, cWire1)
+				steps = append(steps, xStep+yStep)
 			}
 		}
 	}
@@ -22,7 +27,13 @@ func ClosestIntersection(wire1 [][2]int, wire2 [][2]int) int {
 			closestDistance = distance
 		}
 	}
-	return closestDistance
+
+	return closestDistance, minIntSlice(steps)
+}
+
+func minIntSlice(v []int) int {
+	sort.Ints(v)
+	return v[0]
 }
 
 func FindCoordinates(input string) [][2]int {
@@ -60,7 +71,7 @@ func DrawPath(start [2]int, input string) [][2]int {
 	case 'D':
 		y = -1
 	}
-	line := [][2]int{start}
+	var line [][2]int
 	for i := 0; i < distance; i++ {
 		start[0] += x
 		start[1] += y
