eb4232849821d874cd115c8498cc38dbd363e5c4
Alexander Wahl-Rasmussen <a.wahl.r@gmail.com>
2019-12-07 11:16:03 +0100

    added solution for exercise 2

--- day2/main.go.go
+++ day2/main.go.go
@@ -1,5 +1,7 @@
 package day2
 
+import "fmt"
+
 func parseIntcode(input []int) []int {
 	for i := 0; i < len(input); i += 4 {
 		if input[i] == 99 {
@@ -7,8 +9,10 @@ func parseIntcode(input []int) []int {
 		}
 		if input[i] == 1 {
 			input[input[i+3]] = input[input[i+1]] + input[input[i+2]]
-		} else {
+		} else if input[i] == 2 {
 			input[input[i+3]] = input[input[i+1]] * input[input[i+2]]
+		} else {
+			panic(fmt.Sprintf("unknown opcode %d at index %d", input[i], i))
 		}
 	}
 
@@ -17,8 +21,24 @@ func parseIntcode(input []int) []int {
 }
 
 func swapValues(input []int, pos1, pos2 int) []int {
-	result := input
+	result := make([]int, len(input))
+	copy(result, input)
 	result[1] = pos1
 	result[2] = pos2
 	return result
+}
+
+func solve(input []int) int {
+	var actual int
+	for noun := 0; noun <= 99; noun++ {
+		for verb := 0; verb <= 99; verb++ {
+			newArray := swapValues(input, noun, verb)
+			actual := parseIntcode(newArray)
+			if actual[0] == 19690720 {
+				fmt.Println(100*noun + verb)
+				return actual[0]
+			}
+		}
+	}
+	return actual
 }
